3d72c8d101f81b0e33adaa037a1f58befdbb617a
ulugbekovich2113 <2110101@mewuzbekistanuniversity.uz>
2023-04-05 05:17:15 +0500

    a

--- src/App.js
+++ src/App.js
@@ -26,13 +26,13 @@ function App() {
           Learn React
         </a> */}
       {/* </header> */}
-      <Navbar />
-      <Modal />
-      <ToDo/>
+      {/* <Navbar /> */}
+      {/* <Modal /> */}
+      {/* <ToDo/> */}
         
-      <ComponentB/>
+      {/* <ComponentB/> */}
       {/* <Lessson52/> */}
-      <AxiosData/>
+      {/* <AxiosData/> */}
       
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
